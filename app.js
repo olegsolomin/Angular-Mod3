867d0ac816b10828472a7d2472f6f1ab1f46c2fd
Oleg Solomin <solomin.o.n@gmail.com>
2021-06-02 22:05:44 +0300

    uff - got help with controllers

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ angular.module('NarrowItDownApp', [])
     .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
     .directive('menuFromMatched', MenuFromMatched);
 
-    NarrowItDownController.$inject = ['$scope'];
-    function NarrowItDownController($scope) {
-        $scope.colors = ['#ffffff', '#f1f1f1'];
-    }
-
+// Directive
     function MenuFromMatched() {
         var ddo = {
             replace: true,
-            templateUrl: 'MenuFromMatched.html'
+            templateUrl: 'MenuFromMatched.html',
+            //for search
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'list',
+            bindToController: true
         };
 
         return ddo;
     }
-
+    //for search
+    FoundItemsDirectiveController.$inject = ['$scope'];
+    function FoundItemsDirectiveController($scope) {
+        $scope.colors = ['#f1f1f1','#ffffff'];
+        var list = this;
+        //Returns true if list is empty
+        list.checkFoundList = function () {
+            return typeof list.items !== 'undefined' && list.items.length === 0
+        };
+    }
+/* // Display menu from url
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
     var ctrl = this;
@@ -32,12 +42,35 @@ angular.module('NarrowItDownApp', [])
     })
     .catch(function (error) {
         console.log("Something went terribly wrong.");
-    })
-    ctrl.removeItem = function (itemIndex) {
-        ctrl.menu_items.splice(itemIndex, 1);
+    })  */
+// Search
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var ctrl = this;
+
+    ctrl.narrowItDown = function () {
+        if (ctrl.searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+            promise.then(function (response) {
+                ctrl.found = response;
+            })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        } else {
+            ctrl.found = [];
+        }
+
     };
-    }
     
+    //Remove
+    ctrl.removeItem = function (itemIndex) {
+        ctrl.found.splice(itemIndex, 1);  // instead found menu_items in display mode
+    };
+}
+   // service
+/* // service for display from url 
+
 MenuSearchService.$inject =['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
     var service = this;
@@ -50,6 +83,32 @@ function MenuSearchService($http, ApiBasePath) {
         return response;
         
     }
+} */
+// service for search
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json")
+    }).then(function (response) {
+        
+        var foundItems = [];
+        var menuItemsLength = response.data.menu_items.length;
+        
+        for (var i = 0; i < menuItemsLength; i++) {
+            var item = response.data.menu_items[i];
+            if (item.description.indexOf(searchTerm) !== -1) {
+                
+                foundItems.push(item);
+            }
+        };
+        return foundItems;
+    });
+};
 }
+
 
 })();
