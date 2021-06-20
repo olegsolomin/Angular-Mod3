7f586328036769a8086706cc9ffd76e6af0ff8bc
Oleg Solomin <solomin.o.n@gmail.com>
2021-06-20 20:19:42 +0300

    add scope

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ angular.module('NarrowItDownApp', [])
         var ddo = {
             replace: true,
             templateUrl: 'MenuFromMatched.html',
-            // isolated scope
-            // scope: {
-            //     items: '<',
-            //     onRemove: '&'
-            // },
-            //for search
+            scope: {
+               matched: '<',
+               onRemove: '&',
+               // title: @
+            },
             controller: FoundItemsDirectiveController,
             controllerAs: 'list',
             bindToController: true
@@ -25,35 +24,20 @@ angular.module('NarrowItDownApp', [])
 
         return ddo;
     }
-    //for search and remove
-    FoundItemsDirectiveController.$inject = ['$scope'];
-    function FoundItemsDirectiveController($scope) {
-        $scope.colors = ['#f1f1f1','#ffffff'];
+    //for error
+    function FoundItemsDirectiveController() {
         var list = this;
-        //search and remove in directive
-    //     list.IsEmpty = function () {
-    //         if (list.found != undefined && list.found.length === 0) {
-    //             return true;
-    //         }
-    //         return false;
-    //     };
-    //     list.removeItem = function (itemIndex) {
-    //     list.found.splice(itemIndex, 1);
-    // }
+
+        list.IsEmpty = function () {
+            if (list.matched != undefined && list.matched.length === 0) {
+                return true;
+            }
+        };
+        //
+        // var origTitle = "List of Searched Items";
+        // list.title = origTitle + " (" + list.matched.length + " items )";
 }
-/* // Display menu from url
-    NarrowItDownController.$inject = ['MenuSearchService'];
-    function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
 
-    var promise = MenuSearchService.getMatchedMenuItems();
-
-    promise.then(function (response) {
-        ctrl.menu_items = response.data.menu_items;
-    })
-    .catch(function (error) {
-        console.log("Something went terribly wrong.");
-    })  */
 // Search
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
@@ -74,32 +58,17 @@ angular.module('NarrowItDownApp', [])
 
     };
      // search in controller
-    ctrl.IsEmpty = function () {
-        if (ctrl.found != undefined && ctrl.found.length === 0) {
-            return true;
-        }
-    };
+   // ctrl.IsEmpty = function () {
+   //     if (ctrl.found != undefined && ctrl.found.length === 0) {
+   //         return true;
+   //    }
+   //  };
     //Remove in controller
      ctrl.removeItem = function (itemIndex) {
-         ctrl.found.splice(itemIndex, 1);  // instead found menu_items in display mode
+         ctrl.found.splice(itemIndex, 1);
      };
  }
-   // service
-/* // service for display from url 
 
-MenuSearchService.$inject =['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath) {
-    var service = this;
-
-    service.getMatchedMenuItems = function () {
-        var response = $http({
-            method: "GET",
-            url:  (ApiBasePath + "/menu_items.json")
-        });
-        return response;
-        
-    }
-} */
 // service for search
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
@@ -110,15 +79,15 @@ service.getMatchedMenuItems = function (searchTerm) {
         method: "GET",
         url: (ApiBasePath + "/menu_items.json")
     }).then(function (response) {
-        
+
         var foundItems = [];
         var menuItemsLength = response.data.menu_items.length;
-        
+
         for (var i = 0; i < menuItemsLength; i++) {
             var item = response.data.menu_items[i];
             if ((item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)||
                 (item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)) {
-                
+
                 foundItems.push(item);
             }
         };
